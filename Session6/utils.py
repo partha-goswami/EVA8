55943f9cb5b43f8921b5ba24692ef2c933d1184e
Partha Goswami <partha.goswami.1984@outlook.com>
2023-02-01 18:30:03 +0530

    added utility methods in utils.py file
    added utility methods in utils.py file

--- Session6/utils.py
+++ Session6/utils.py
@@ -62,3 +62,71 @@ def get_config_values():
       'gray_probability': 0.15
   }
   return dict_config_values
+
+
+def apply_albumentation(config_dict):
+  '''
+  Kept separate method to apply albumentation. As it returns the transformation applied in terms of dictionary.
+  Our next dataset and dataloaders would use the transformation by calling this method.
+  '''
+  cifar10_mean,cifar10_std = get_cifar10_stats()
+
+  train_transforms = albumentations.Compose([albumentations.HorizontalFlip(p=config_dict['albumentation']['horizontalFlip_probability']),
+                                  albumentations.ShiftScaleRotate(shift_limit=config_dict['albumentation']['shiftScaleRotate_shift_limit'], 
+                                                                  scale_limit=config_dict['albumentation']['shiftScaleRotate_scale_limit'],
+                                                                  rotate_limit=config_dict['albumentation']['shiftScaleRotate_rotate_limit'],
+                                                                  p=config_dict['albumentation']['shiftScaleRotate_probability']),
+                                  albumentations.CoarseDropout(max_holes=config_dict['albumentation']['coarseDropout_max_holes'],
+                                                               min_holes =config_dict['albumentation']['coarseDropout_min_holes'], 
+                                                               max_height=config_dict['albumentation']['coarseDropout_max_height'], 
+                                                               max_width=config_dict['albumentation']['coarseDropout_max_width'], 
+                                  p=config_dict['albumentation']['coarseDropout_cutout_probability'],fill_value=tuple([x * 255.0 for x in cifar10_mean]),
+                                  min_height=config_dict['albumentation']['coarseDropout_min_height'], min_width=config_dict['albumentation']['coarseDropout_min_width']),
+                                  albumentations.ColorJitter(p=config_dict['albumentation']['colorJitter_probability'],
+                                                             brightness=config_dict['albumentation']['colorJitter_brightness'], 
+                                                             contrast=config_dict['albumentation']['colorJitter_contrast'], 
+                                                             saturation=config_dict['albumentation']['colorJitter_saturation'], hue=config_dict['albumentation']['colorJitter_hue']),
+                                  albumentations.ToGray(p=config_dict['albumentation']['gray_probability']),
+                                  albumentations.Normalize(mean=cifar10_mean, std=cifar10_std,always_apply=True),
+                                  ToTensorV2()
+                                ])
+
+  test_transforms = albumentations.Compose([albumentations.Normalize(mean=cifar10_mean, std=cifar10_std, always_apply=True),
+                                 ToTensorV2()])
+  return lambda img:train_transforms(image=np.array(img))["image"],lambda img:test_transforms(image=np.array(img))["image"]
+
+def get_data_loaders(config_dict):
+  '''
+  This method applies albumentation transforms and returns the train and test dataloaders
+  : param config_dict: dictionary of config values
+  '''
+  train_transforms, test_transforms = apply_albumentation(config_dict)  
+  
+
+  trainset = datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=train_transforms)  
+        
+  testset  = datasets.CIFAR10(root='./data', train=False,
+                                        download=True, transform=test_transforms)
+
+  train_loader = torch.utils.data.DataLoader(trainset, 
+                                                  batch_size=config_dict['batch_size'], 
+                                                  shuffle=True,
+                                                  num_workers=config_dict['no_of_workers'], 
+                                                  pin_memory=config_dict['pin_memory'])
+  test_loader = torch.utils.data.DataLoader(testset, 
+                                                 batch_size=config_dict['batch_size'], 
+                                                  shuffle=True,
+                                                  num_workers=config_dict['no_of_workers'], 
+                                                  pin_memory=config_dict['pin_memory'])
+  return train_loader, test_loader
+
+def print_model_summary(model, device):
+    '''
+      THis method returns the model summary.
+
+      :param model: model
+      :param device: device
+    '''
+    cifar_model = model.to(device)
+    return summary(cifar_model, input_size=(3, 32, 32))
